b3cda28f779fb45cb4210de8c6c0d2c77b655ba9
atomicNum29 <atomicw63.546@gmail.com>
2023-02-08 22:49:02 +0900

    보안 레지스트리 수정 완료

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 import win32com.client as win32
 
 hwp=win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
+hwp.RegisterModule("FilePathCheckDLL", "AutomationModule")
 hwp.XHwpWindows.Item(0).Visible = True
 
 def printhwp(s):
