ca8d9052e6d222bfa08ebe9f77484f95f9ab2206
atomicNum29 <atomicw63.546@gmail.com>
2023-02-08 21:53:48 +0900

    이미지 입력 성공

--- practice.py
+++ practice.py
@@ -5,5 +5,5 @@ hwp.XHwpWindows.Item(0).Visible = True
 
 hwp.HAction.GetDefault("InsertText", hwp.HParameterSet.HInsertText.HSet)
 hwp.HParameterSet.HInsertText.Text = "ㅁㄴㅇㄹ"
-hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet);
-# hwp.Open("hwp 파일 경로","HWP","forceopen:true")
+hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet)
+hwp.InsertPicture("D:\\hangle_automation\\data\\스크린샷(1).png", Embedded=True) # 이미지 삽입
