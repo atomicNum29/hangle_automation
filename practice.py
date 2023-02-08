0adfef3b594f3b8e7f40eeb1b30c18fd23001789
atomicNum29 <atomicw63.546@gmail.com>
2023-02-08 19:47:48 +0900

    새 빈 파일에 'ㅁㄴㅇㄹ' 작성 성공

--- practice.py
+++ practice.py
@@ -2,4 +2,8 @@ import win32com.client as win32
 
 hwp=win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
 hwp.XHwpWindows.Item(0).Visible = True
+
+hwp.HAction.GetDefault("InsertText", hwp.HParameterSet.HInsertText.HSet)
+hwp.HParameterSet.HInsertText.Text = "ㅁㄴㅇㄹ"
+hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet);
 # hwp.Open("hwp 파일 경로","HWP","forceopen:true")
