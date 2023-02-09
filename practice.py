d4ffeb02a11d6c2e7cc6f5ee1c1fbe7643e7e293
atomicNum29 <atomicw63.546@gmail.com>
2023-02-09 16:11:28 +0900

    코드 간략화

--- practice.py
+++ practice.py
@@ -1,40 +1,35 @@
 import win32com.client as win32
 
 hwp=win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
+# 보안 창 생략
 hwp.RegisterModule("FilePathCheckDLL", "AutomationModule")
+# 한글 프로그램 보여주기
 hwp.XHwpWindows.Item(0).Visible = True
 
+# 현재 커서 위치에 s 문자열 작성
 def printhwp(s):
     hwp.HAction.GetDefault("InsertText", hwp.HParameterSet.HInsertText.HSet)
     hwp.HParameterSet.HInsertText.Text = s
     hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet)
 
+# 특정 한글 파일 열기
 hwp.Open("D:\hangle_automation\data\(매뉴얼) 운영_매뉴얼_ver1_최종 (1) - 복사본.hwp","HWP","forceopen:true")
-
+# 특정 이미지 삽입 후 잘라내기
+# 클립보드에 이미지를 저장함.
 hwp.InsertPicture("D:\hangle_automation\data\스크린샷(1).png", Embedded=True) # 이미지 삽입
 hwp.FindCtrl() # 이미지 선택 (커서에서 인접한 개체 선택)
 hwp.HAction.Run("Cut") # 잘라내기
-while True:
-	# 이미지 삽입할 위치 찾기
-	hwp.HAction.GetDefault("RepeatFind", hwp.HParameterSet.HFindReplace.HSet)
-	hwp.HParameterSet.HFindReplace.FindString = "image1"
-	hwp.HParameterSet.HFindReplace.IgnoreMessage = 1
-	result = hwp.HAction.Execute("RepeatFind", hwp.HParameterSet.HFindReplace.HSet)
-
-	# 다 바꿨으면 종료
-	if result == False:
-		break
-
-	# 붙여넣기
-	hwp.HAction.GetDefault("Paste", hwp.HParameterSet.HSelectionOpt.HSet)
-	hwp.HAction.Execute("Paste", hwp.HParameterSet.HSelectionOpt.HSet)
-
+# 이미지 삽입할 위치 찾기; image1 위치를 찾는다.
+hwp.HAction.GetDefault("RepeatFind", hwp.HParameterSet.HFindReplace.HSet)
+hwp.HParameterSet.HFindReplace.FindString = "image1"
+hwp.HParameterSet.HFindReplace.IgnoreMessage = 1
+result = hwp.HAction.Execute("RepeatFind", hwp.HParameterSet.HFindReplace.HSet)
+# 붙여넣기
+hwp.HAction.GetDefault("Paste", hwp.HParameterSet.HSelectionOpt.HSet)
+hwp.HAction.Execute("Paste", hwp.HParameterSet.HSelectionOpt.HSet)
+# 붙여넣은 이미지 속성 변경으로 크기 조정
 hwp.FindCtrl()
 hwp.HAction.GetDefault("ShapeObjDialog", hwp.HParameterSet.HShapeObject.HSet)
 hwp.HParameterSet.HShapeObject.Height = 17007
 hwp.HParameterSet.HShapeObject.Width = 22677
-# hwp.HParameterSet.HShapeObject.HorzRelTo = hwp.HorzRel("Para")
-# hwp.HParameterSet.HShapeObject.HSet.SetItem("ShapeType", 1)
-print(hwp.HAction.Execute("ShapeObjDialog", hwp.HParameterSet.HShapeObject.HSet))
-
-# hwp.Save
+hwp.HAction.Execute("ShapeObjDialog", hwp.HParameterSet.HShapeObject.HSet)
