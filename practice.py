ffa96b38ee6408bb223cf96f47df4685862981c4
atomicNum29 <atomicw63.546@gmail.com>
2023-02-09 16:23:41 +0900

    특정 파일 이름으로 저장

--- practice.py
+++ practice.py
@@ -1,5 +1,12 @@
 import win32com.client as win32
 
+data_path = "D:/hangle_automation/data"
+original_hwpfile_name = "(매뉴얼) 운영_매뉴얼_ver1_최종 (1) - 복사본.hwp"
+image_file_name = "스크린샷(1).png"
+
+output_path = "D:/hangle_automation/output"
+output_file_name = "저장.hwp"
+
 hwp=win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
 # 보안 창 생략
 hwp.RegisterModule("FilePathCheckDLL", "AutomationModule")
@@ -13,10 +20,10 @@ def printhwp(s):
     hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet)
 
 # 특정 한글 파일 열기
-hwp.Open("D:\hangle_automation\data\(매뉴얼) 운영_매뉴얼_ver1_최종 (1) - 복사본.hwp","HWP","forceopen:true")
+hwp.Open(data_path + '/' + original_hwpfile_name,"HWP","forceopen:true")
 # 특정 이미지 삽입 후 잘라내기
 # 클립보드에 이미지를 저장함.
-hwp.InsertPicture("D:\hangle_automation\data\스크린샷(1).png", Embedded=True) # 이미지 삽입
+hwp.InsertPicture(data_path + '/' + image_file_name, Embedded=True) # 이미지 삽입
 hwp.FindCtrl() # 이미지 선택 (커서에서 인접한 개체 선택)
 hwp.HAction.Run("Cut") # 잘라내기
 # 이미지 삽입할 위치 찾기; image1 위치를 찾는다.
@@ -33,3 +40,7 @@ hwp.HAction.GetDefault("ShapeObjDialog", hwp.HParameterSet.HShapeObject.HSet)
 hwp.HParameterSet.HShapeObject.Height = 17007
 hwp.HParameterSet.HShapeObject.Width = 22677
 hwp.HAction.Execute("ShapeObjDialog", hwp.HParameterSet.HShapeObject.HSet)
+ 
+# 수정 완료한 파일 저장 
+hwp.SaveAs(output_path + '/' + output_file_name)
+hwp.Quit()
